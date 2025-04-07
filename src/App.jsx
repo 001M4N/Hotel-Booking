73f2cf97f302f45cb4146996ee52b04cdca70893
001M4N <imangholiali2000@gmail.com>
2025-04-07 13:07:42 +0330

    starting the hotel-booking project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <h1>start of the hotel-booking project</h1>
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App;
