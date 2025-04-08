a67f778b9a029a76ed482bb5e703b5e30e39c716
001M4N <imangholiali2000@gmail.com>
2025-04-08 15:32:23 +0330

    Header Component is done

--- hotel-booking/src/App.jsx
+++ hotel-booking/src/App.jsx
@@ -1,10 +1,9 @@
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <h1 className="text-blue-100">start of the hotel-booking project</h1>
       {/* <Footer /> */}
     </>
---
+++ hotel-booking/src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">Hotel Booking</a>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Sign in/up</a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
