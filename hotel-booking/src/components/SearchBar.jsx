60cf5f1dac4fd3b55a07892d45f8833314e0d7a0
001M4N <imangholiali2000@gmail.com>
2025-04-08 17:19:03 +0330

    SearchBar Component is done

---
+++ hotel-booking/src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+function SearchBar() {
+  return (
+    <div className="container h-30 flex flex-col justify-center gap-x-5 gap-y-3 items-center sm:flex-row">
+      <label className="input">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input type="search" required placeholder="Search Hotel Name" />
+      </label>
+
+      <select defaultValue="Tehran" className="select sm:w-3xs">
+        <option>Tehran</option>
+        <option>Kish</option>
+      </select>
+    </div>
+  );
+}
+export default SearchBar;
--- hotel-booking/src/pages/Home.jsx
+++ hotel-booking/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
 import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <Header />
+      <SearchBar />
       <Footer />
     </>
   );
