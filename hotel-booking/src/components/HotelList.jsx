742cd09390147c30dae1c4e922a8b0b6775d7585
001M4N <imangholiali2000@gmail.com>
2025-04-12 18:24:56 +0330

    making a divder for each section: hotels and popular hotels

--- hotel-booking/src/components/HotelList.jsx
+++ hotel-booking/src/components/HotelList.jsx
@@ -3,7 +3,9 @@ import HotelCard from "./HotelCard";
 function HotelList() {
   return (
     <div className="my-10">
-      <h2 className="text-xl font-bold text-center mb-6 sm:mb-4">Hotels</h2>
+      <div className="divider divider mb-6">
+        <h2 className="text-xl font-bold text-center">Hotels</h2>
+      </div>
       <div className="m-5 grid grid-cols-2 gap-5 place-items-center sm:grid-cols-5 xlg:grid-cols-6">
         <HotelCard cardSize="w-50 sm:w-60" cardColor="bg-slate-700" />
         <HotelCard cardSize="w-50 sm:w-60" cardColor="bg-slate-700" />
--- hotel-booking/src/components/PopularHotels.jsx
+++ hotel-booking/src/components/PopularHotels.jsx
@@ -3,9 +3,9 @@ import HotelCard from "./HotelCard";
 function PopularHotels() {
   return (
     <div className="w-full mt-6">
-      <h2 className="text-xl font-bold text-center mb-6 sm:mb-4">
-        Popular Hotels
-      </h2>
+      <div className="divider mb-6">
+        <h2 className="text-xl font-bold text-center">Popular Hotels</h2>
+      </div>
       <div className="grid grid-cols-1 place-items-center md:grid-cols-3">
         <HotelCard
           cardSize="w-100 sm:w-90"
