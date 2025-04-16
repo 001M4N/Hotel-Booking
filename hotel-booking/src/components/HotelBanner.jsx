9b36908404899dbe06d14ed8a0cbd20f5e2741d9
001M4N <imangholiali2000@gmail.com>
2025-04-16 18:44:07 +0330

    minor chaneg

--- hotel-booking/src/components/HotelBanner.jsx
+++ hotel-booking/src/components/HotelBanner.jsx
@@ -3,7 +3,7 @@ import HotelRating from "./HotelRating";
 export default function Hotelbanner() {
   return (
     <div className="hero w-full h-80 bg-[url(../../public/assets/placeHolderImage.webp)] relative">
-      <div className="w-full h-8 absolute bottom-0 bg-black opacity-80 px-2 py-1 flex justify-between">
+      <div className="w-full h-10 absolute bottom-0 bg-black opacity-80 px-2 py-2 flex justify-between">
         <h2 className="text-xl">Hotel Abbasi</h2>
         <HotelRating />
       </div>
