d13def55217ed576f4587b5f66c81eb42e21a904
001M4N <imangholiali2000@gmail.com>
2025-04-22 16:26:00 +0330

    made a custom hook for fetching APIs

---
+++ hotel-booking/src/hooks/useAPICall.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export default function useAPICall({ url }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Loading Failed");
+        }
+        const json = await response.json();
+        setData(json);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
+}
