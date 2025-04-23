30011410b03d136c4ccfb891e35496dcf8361cce
001M4N <imangholiali2000@gmail.com>
2025-04-23 23:54:23 +0330

    Carousel Component is almost done, just applying some animation for smooth transition is required

--- hotel-booking/src/components/Carousel.jsx
+++ hotel-booking/src/components/Carousel.jsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
 import PropertyCard from "./PropertyCard";
 
-export default function Carousel() {
+export default function Carousel({ items }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [width, setWidth] = useState(window.innerWidth);
   const numItemsPerSlide =
     width >= 1600 ? 4 : width >= 1200 ? 3 : width >= 800 ? 2 : 1;
+
+  const arrayLength = items.length;
+  const selectedIndices = Array.from({ length: numItemsPerSlide }, (_, i) => {
+    return (currentIndex + i) % arrayLength;
+  });
+
+  const data = items.filter((_, i) => {
+    return selectedIndices.includes(i);
+  });
 
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
@@ -13,44 +22,30 @@ export default function Carousel() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const data = [...Array(numItemsPerSlide)].map(() => {
-    return (
-      <PropertyCard
-        isPopular={true}
-        cardTitle="Hotel Abbasi"
-        cardDescription="Total Price = $100"
-        buttonText="Hotel Page"
-      />
-    );
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      window.innerWidth >= 1024
-        ? setNumItems(4)
-        : window.innerWidth >= 768
-        ? setNumItems(3)
-        : setNumItems(1);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const nextCard = () => {};
-
-  const previousCard = () => {};
+  const handlePrevClick = () => {
+    currentIndex === 0
+      ? setCurrentIndex(arrayLength - 1)
+      : setCurrentIndex(currentIndex - 1);
+  };
+  const handlenextlick = () => {
+    currentIndex === arrayLength
+      ? setCurrentIndex(0)
+      : setCurrentIndex(currentIndex + 1);
+  };
 
   return (
     <div className="bg-white w-full flex justify-center gap-x-20 p-10 relative">
-      <button className="btn btn-lg absolute bottom-1/2 left-2 opacity-50 rounded-full z-100">
+      <button
+        onClick={handlePrevClick}
+        className="btn btn-lg absolute bottom-1/2 left-2 opacity-50 rounded-full z-100"
+      >
         &lt;
       </button>
-
       {data}
-
-      <button className="btn btn-lg absolute bottom-1/2 right-2 opacity-50 rounded-full z-100">
+      <button
+        onClick={handlenextlick}
+        className="btn btn-lg absolute bottom-1/2 right-2 opacity-50 rounded-full z-100"
+      >
         &gt;
       </button>
     </div>
