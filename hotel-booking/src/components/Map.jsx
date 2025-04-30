a3507515947763ee87ed9d6262b33b74c40523cd
001M4N <imangholiali2000@gmail.com>
2025-04-30 14:16:06 +0330

    add lang and lat and name for popup to the map

--- hotel-booking/src/components/Map.jsx
+++ hotel-booking/src/components/Map.jsx
@@ -5,16 +5,16 @@ export default function Map({ lat, lang, name }) {
   return (
     <div className="h-80 w-full m-5">
       <MapContainer
-        center={[51.505, -0.09]}
+        center={[{ lat }, { lang }]}
         zoom={13}
-        style={{ height: "100%", width: "100%", borderRadius: "50%" }}
+        style={{ height: "100%", width: "100%", borderRadius: "50px" }}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
         <Marker position={[51.505, -0.09]}>
-          <Popup>{}</Popup>
+          <Popup>{name}</Popup>
         </Marker>
       </MapContainer>
     </div>
