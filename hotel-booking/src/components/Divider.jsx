b374f3a34300838b976dd944bde75c4192310221
001M4N <imangholiali2000@gmail.com>
2025-05-02 13:07:13 +0330

    change the Divider, so it can show only a horizantal line without any title for cases that declare line prop as true

--- hotel-booking/src/components/Divider.jsx
+++ hotel-booking/src/components/Divider.jsx
@@ -1,5 +1,7 @@
-export default function Divider({ title }) {
-  return (
+export default function Divider({ title, line = false }) {
+  return line ? (
+    <div className="divider mb-6"></div>
+  ) : (
     <div className="divider mb-6">
       <h2 className="text-xl font-bold text-center">{title}</h2>
     </div>
--- hotel-booking/src/components/Filter.jsx
+++ hotel-booking/src/components/Filter.jsx
@@ -3,7 +3,7 @@ import Divider from "./Divider";
 export default function Filter({ children }) {
   return (
     <>
-      <Divider title={"Filters"} />
+      <Divider line={true} />
       <div className="min-h-30 flex flex-col justify-center items-center gap-7 lg:flex-row my-5">
         {children}
       </div>
